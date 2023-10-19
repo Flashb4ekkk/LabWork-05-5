9b3d107ec19b254340222886fc25900ebf2b3221
Bethakzs <romanwdwd@gmail.com>
2023-10-19 13:18:19 +0300

    final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,27 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.print("n = ");
         int n = scan.nextInt();
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print(pascal(i, j) + "  ");
+            System.out.print("  ");
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("   ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                int pascalValue = pascal(i, j);
+                if (pascalValue != 0) {
+                    System.out.print(pascalValue + "     ");
+                }
             }
             System.out.println();
         }
     }
+
     public static int pascal(int i, int j) {
         if (j == 0 || j == i) {
             return 1;
+        } else if (j < 0 || j > i) {
+            return 0;
         } else {
             return pascal(i - 1, j - 1) + pascal(i - 1, j);
         }
